06da6a6b6cebf0ca6e295f3ef4db7e47a8a3bd08
Nam Nguyen <65736607+namnguyen1994@users.noreply.github.com>
2021-09-12 22:18:51 -0400

    Add files via upload

---
+++ Header.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+function Header(){
+    return (
+        <header>
+            <img 
+                src="https://www.prospectmagazine.co.uk/content/uploads/2016/11/Pepedankmeems420blazeitohbabyatripledankaestheticafkeepit100.jpg"
+                alt="Problem?" 
+            />
+            <p> Meme Factory </p>
+        </header>
+    )
+}
+export default Header
